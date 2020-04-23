45494a40f6b154dde2b73e5d1abbfe10f9e0a79b
Ben Fradet <benjamin.fradet@gmail.com>
2020-04-23 10:22:08 +0200

    Add munit docker test kit

---
+++ src/main/scala/com/github/benfradet/docker/munit/DockerTestKit.scala
@@ -0,0 +1,11 @@
+package com.github.benfradet.docker.munit
+
+import com.whisk.docker.DockerKit
+
+trait DockerTestKit extends DockerKit { self: munit.FunSuite =>
+  override def beforeAll(): Unit =
+    startAllOrFail()
+
+  override def afterAll(): Unit =
+    stopAllQuietly()
+}
