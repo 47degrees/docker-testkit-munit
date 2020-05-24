a0a39d4de4db3158269df2852139ecb366f69826
Alejandro Hernández <info@alejandrohdezma.com>
2020-05-24 08:39:40 +0200

    Update dependencies to latest version

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -1,6 +1,6 @@
+import com.alejandrohdezma.sbt.github.SbtGithubPlugin
 import sbt.Keys._
 import sbt._
-import com.alejandrohdezma.sbt.github.SbtGithubPlugin
 
 object ProjectPlugin extends AutoPlugin {
 
@@ -12,13 +12,13 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val testkitSettings = Seq(
       libraryDependencies ++= Seq(
-        "org.scalameta" %% "munit"                           % "0.7.4",
+        "org.scalameta" %% "munit"                           % "0.7.7",
         "com.whisk"     %% "docker-testkit-core"             % "0.9.9",
-        "com.whisk"     %% "docker-testkit-impl-spotify"     % "0.9.9"    % Test,
-        "com.whisk"     %% "docker-testkit-impl-docker-java" % "0.9.9"    % Test,
-        "com.whisk"     %% "docker-testkit-samples"          % "0.9.9"    % Test,
-        "ch.qos.logback" % "logback-classic"                 % "1.2.3"    % Test,
-        "org.postgresql" % "postgresql"                      % "9.4.1212" % Test
+        "com.whisk"     %% "docker-testkit-impl-spotify"     % "0.9.9"   % Test,
+        "com.whisk"     %% "docker-testkit-impl-docker-java" % "0.9.9"   % Test,
+        "com.whisk"     %% "docker-testkit-samples"          % "0.9.9"   % Test,
+        "ch.qos.logback" % "logback-classic"                 % "1.2.3"   % Test,
+        "org.postgresql" % "postgresql"                      % "42.2.12" % Test
       )
     )
   }
