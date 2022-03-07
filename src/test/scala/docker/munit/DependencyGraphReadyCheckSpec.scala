db8361cd2a8f3ff4c5730eb3cbf258ae54768fff
47erbot <developer@47deg.com>
2022-03-07 17:24:45 +0100

    Update logback-classic to 1.2.11

--- src/test/scala/docker/munit/DependencyGraphReadyCheckSpec.scala
+++ src/test/scala/docker/munit/DependencyGraphReadyCheckSpec.scala
@@ -28,6 +28,10 @@ import scala.language.postfixOps
 class DependencyGraphReadyCheckSpec extends FunSuite with DockerKitSpotify {
 
   override val StartContainersTimeout = 45 seconds
+  override val StopContainersTimeout  = 45 seconds
+
+  override def munitTimeout: Duration =
+    StartContainersTimeout.plus(StopContainersTimeout).plus(PullImagesTimeout).plus(30.seconds)
 
   private lazy val log = LoggerFactory.getLogger(this.getClass)
 
